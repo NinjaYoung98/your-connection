185a386800b2e597433d91954ce515944d6462cc
ChoChanYoung <whcksdud8@gmail.com>
2023-08-23 02:46:41 +0900

    [#1] chore : 프로젝트 초기 환경 세팅
    - spring boot 2.7.14
    - MySQL 8.0.21
    - Jdk 17 (liberica 17)
    - spring data JPA
    - git ignore 추가
    - github-flow 브랜치 전략 수립

---
+++ src/main/java/com/sns/yourconnection/YourConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.sns.yourconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourConnectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sns/yourconnection/YourConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sns.yourconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourConnectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
