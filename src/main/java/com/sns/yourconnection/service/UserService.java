9ac6cf9495e6f90595fa8911a99ebe1c9958fa3a
ChoChanYoung <whcksdud8@gmail.com>
2023-08-25 01:16:29 +0900

    [#15] feat : security authentication 기본 구성
    - security config 구성
    - entry point 및 jwt filter 클래스 생성 but 구현 x
    - "/public-api/**" 일 경우 인증절차 x
    - "/api/**" 일 경우 인증절차 o

--- src/main/java/com/sns/yourconnection/service/UserService.java
+++ src/main/java/com/sns/yourconnection/service/UserService.java
@@ -9,9 +9,7 @@ import com.sns.yourconnection.exception.ErrorCode;
 import com.sns.yourconnection.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
