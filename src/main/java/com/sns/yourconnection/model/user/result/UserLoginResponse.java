72cd1681f03efce9d952e97afd07cdfd35bb8d72
ChoChanYoung <whcksdud8@gmail.com>
2023-08-24 23:51:12 +0900

    [#9] feat : 간단한 로그인 기능 구현
    - spring security 도입 후 api 인증 절차를 위해
      로그인 기능을 public api로 분류
    - username 중복 체크 및 password 검증
       UserService에서 구현

--- src/main/java/com/sns/yourconnection/model/user/result/UserLoginResponse.java
+++ src/main/java/com/sns/yourconnection/model/user/result/UserLoginResponse.java
@@ -1,6 +1,5 @@
 package com.sns.yourconnection.model.user.result;
 
-import com.sns.yourconnection.security.token.AccessToken;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,11 +8,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 public class UserLoginResponse {
-    private String accessToken;
-    private String grantType;
-    private Long expiresIn;
 
-    public static UserLoginResponse of(AccessToken accessToken) {
-        return new UserLoginResponse(accessToken.getAccessToken(), accessToken.getGrantType(), accessToken.getExpiresIn());
+    private String accessToken;
+
+
+    public static UserLoginResponse of(String accessToken) {
+        return new UserLoginResponse(accessToken);
     }
 }
---
+++ src/main/java/com/sns/yourconnection/service/UserService.java
@@ -0,0 +1,60 @@
+package com.sns.yourconnection.service;
+
+import com.sns.yourconnection.model.user.dto.User;
+import com.sns.yourconnection.model.user.param.UserJoinRequest;
+import com.sns.yourconnection.model.user.param.UserLoginRequest;
+import com.sns.yourconnection.model.user.entity.UserEntity;
+import com.sns.yourconnection.exception.AppException;
+import com.sns.yourconnection.exception.ErrorCode;
+import com.sns.yourconnection.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User join(UserJoinRequest userJoinRequest) {
+        /*
+        회원 정보(username, password, nickname)를 등록한다.
+            -  username이 이미 존재할 경우 에러 반환
+         */
+        DuplicateUsername(userJoinRequest.getUsername());
+
+        UserEntity userEntity = UserEntity.of(
+            userJoinRequest.getUsername(), userJoinRequest.getPassword(),
+            userJoinRequest.getNickname());
+        log.info("UserEntity has created for join with ID: {} username: nickname: {}",
+            userEntity.getId(), userEntity.getUsername(), userEntity.getNickname());
+
+        userRepository.save(userEntity);
+        return User.fromEntity(userEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public String login(UserLoginRequest userLoginRequest) {
+        User user = userRepository.findByUsername(userLoginRequest.getUsername())
+            .map(User::fromEntity)
+            .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        validatePassword(userLoginRequest, user);
+        return "accessToken";
+    }
+
+    private void validatePassword(UserLoginRequest userLoginRequest, User user) {
+        if (!userLoginRequest.getPassword().equals(user.getPassword())) {
+            throw new AppException(ErrorCode.INVALID_PASSWORD);
+        }
+    }
+
+    public void DuplicateUsername(String username) {
+        userRepository.findByUsername(username).ifPresent(userEntity -> {
+            throw new AppException(ErrorCode.DUPLICATED_USERNAME);
+        });
+    }
+}
