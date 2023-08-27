b5dc43c9238d45141ca886a69b9dd901ffc5aa39
ChoChanYoung <whcksdud8@gmail.com>
2023-08-27 15:33:44 +0900

    [#45] feat : rate limit 시스템 구축
    - 특정 기간 동안 클라이언트의 요청 수를 제어하기
     위해 인터셉터에서 rate limit system
     구현하였습니다
    - 클라이언트는  IP 주소로 식별됩니다.
    - 각 버킷은 100개의 토큰 용량을 가지고 있으며, 1초마다 50개의 토큰으로 리필됩니다.
    - 1시간 안에 10번 이상의 rate limit 발생시
      텔레그램으로 알림
    - 1시간 동안의 요청은 해당 시간 창 내에서 카운트
       되며, 한 시간이 경과하면 카운터가
       초기화됩니다.

---
+++ src/main/java/com/sns/yourconnection/common/interceptor/RateLimitingInterceptor.java
@@ -0,0 +1,96 @@
+package com.sns.yourconnection.common.interceptor;
+
+import com.sns.yourconnection.exception.ErrorCode;
+import com.sns.yourconnection.utils.RateLimitUtil;
+import com.sns.yourconnection.utils.HttpReqRespUtil;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
+import io.github.bucket4j.Refill;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class RateLimitingInterceptor implements HandlerInterceptor {
+    private final Long BUCKET_CAPACITY = 100L;
+    private final Long BUCKET_TOKENS = 50L;
+    private final Duration CALLS_IN_SECONDS = Duration.ofSeconds(1);
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+    private final RateLimitUtil rateLimitUtil;
+
+    /**
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @param handler  chosen handler to execute, for type and/or instance evaluation
+     * @return
+     * @throws Exception
+     * @apiNote `Retry-After` 설명: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+     */
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String clientIp = HttpReqRespUtil.getClientIp(request);
+        Bucket bucket = cache.computeIfAbsent(clientIp, key -> newBucket());
+        ConsumptionProbe consumptionProbe = bucket.tryConsumeAndReturnRemaining(1);
+        if (isRateLimitExceeded(request, response, clientIp, consumptionProbe)) return false;
+        return true;
+    }
+
+    /**
+     * @apiNote rate limit 발생여부에 따른 각각의 success, error response 를 생성 및 반환 합니다.
+     * * 'RateLimitCounter.checkLimitReachedThreshold()' 1시간에 10 번 이상의 rate limit 발생하는지 check 합니다.*
+     */
+    private boolean isRateLimitExceeded(HttpServletRequest request, HttpServletResponse response, String clientIp, ConsumptionProbe consumptionProbe) {
+        if (!consumptionProbe.isConsumed()) {
+            errorResponse(response, consumptionProbe);
+            log.warn("rate limit exceeded for client IP :{}  Refill in {} seconds  Request details: method = {} URI = {}"
+                    , clientIp, getRoundedSecondsToWaitForRefill(consumptionProbe), request.getMethod(), request.getRequestURI());
+            rateLimitUtil.checkLimitReachedThreshold(clientIp); // 만약에 1시간에 10번 이상의 Rate Limit 에러를 발생시키는 유저가 있다면 텔레그램 알림.
+            return true;
+        }
+        successResponse(response, consumptionProbe);
+        log.info("remaining token: {}", consumptionProbe.getRemainingTokens());
+        return false;
+    }
+
+    private Bucket newBucket() {
+        // 버킷의 총 크기 = 100 , 한 번에 충전되는 토큰 = 50 , 버킷 충전 시간 = 1초
+        return Bucket.builder()
+                .addLimit(Bandwidth.classic(
+                        BUCKET_CAPACITY, Refill.intervally(
+                                BUCKET_TOKENS, CALLS_IN_SECONDS)))
+                .build();
+    }
+
+    /**
+     * @apiNote 'X-RateLimit-RetryAfter', 'X-RateLimit-Limit', 'X-RateLimit-Remaining'
+     * 참고: https://sendbird.com/docs/chat/v3/platform-api/application/understanding-rate-limits/rate-limits
+     */
+    //TODO: response message ( custom response 객체 생성 후 구현 예정)
+    private void successResponse(HttpServletResponse response, ConsumptionProbe consumptionProbe) {
+        response.setHeader("X-RateLimit-Remaining", Long.toString(consumptionProbe.getRemainingTokens()));
+        response.setHeader("X-RateLimit-Limit", BUCKET_CAPACITY + ";w=" + CALLS_IN_SECONDS.getSeconds());
+    }
+
+    private void errorResponse(HttpServletResponse response, ConsumptionProbe consumptionProbe) {
+        response.setHeader("X-RateLimit-RetryAfter", Float.toString(getRoundedSecondsToWaitForRefill(consumptionProbe)));
+        response.setHeader("X-RateLimit-Limit", BUCKET_CAPACITY + ";w=" + CALLS_IN_SECONDS.getSeconds());
+        response.setStatus(ErrorCode.TOO_MANY_REQUESTS.getHttpStatus().value());
+    }
+
+    private float getRoundedSecondsToWaitForRefill(ConsumptionProbe consumptionProbe) {
+        float secondsToWaitForRefill = (float) TimeUnit.NANOSECONDS.toMillis(consumptionProbe.getNanosToWaitForRefill()) / 1000;
+        return (float) Math.round(secondsToWaitForRefill * 10) / 10;
+    }
+}
