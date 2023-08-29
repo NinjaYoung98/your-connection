d9db02711bf194b8dbfa0691b30c8156026a5284
ChoChanYoung <whcksdud8@gmail.com>
2023-08-29 20:20:20 +0900

    [#49] feat : JavaMailSender 및 SMTP 메일 속성 설정
    - JavaMailSender를 생성하고 초기화하는 Bean
     정의를 추가했습니다.
    - SMTP 서버의 호스트, 포트, 사용자 이름,
     비밀번호 및 인코딩 등 필요한 정보들은
     SmtpMailProperties에서 가져와 설정합니다.
    - SMTP 메일 서비스에 필요한 여러 속성들(인증,
      TLS 활성화 여부 등)도 함께 설정하였습니다.

---
+++ src/main/java/com/sns/yourconnection/common/configuration/SmtpMailConfig.java
@@ -0,0 +1,46 @@
+package com.sns.yourconnection.common.configuration;
+
+import com.sns.yourconnection.service.thirdparty.email.SmtpMailProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SmtpMailConfig {
+
+    private final SmtpMailProperties smtpMailProperties;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(smtpMailProperties.getHost());
+        mailSender.setPort(smtpMailProperties.getPort());
+        mailSender.setUsername(smtpMailProperties.getUsername());
+        mailSender.setPassword(smtpMailProperties.getPassword());
+        mailSender.setDefaultEncoding(smtpMailProperties.getEncode());
+        mailSender.setJavaMailProperties(getProperties());
+        return mailSender;
+    }
+
+    /**
+     * @apiNote Properties key 값 ( mail.smtp) 참고:
+     * https://javaee.github.io/javamail/docs/api/com/sun/mail/smtp/package-summary.html#package.description
+     */
+    public Properties getProperties() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", smtpMailProperties.isAuth());
+        properties.put("mail.smtp.starttls.enable", smtpMailProperties.isStarttlsEnable());
+        properties.put("mail.smtp.starttls.required", smtpMailProperties.isStarttlsRequired());
+        properties.put("mail.smtp.connectiontimeout", smtpMailProperties.getConnectionTimeout());
+        properties.put("mail.smtp.timeout", smtpMailProperties.getTimeout());
+        properties.put("mail.smtp.writetimeout", smtpMailProperties.getWriteTimeout());
+        return properties;
+    }
+}
