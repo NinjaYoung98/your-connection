fd61ba53c3c60fdf0433f63ced3f5b39cb17207f
ChoChanYoung <whcksdud8@gmail.com>
2023-08-31 00:56:50 +0900

    [#58] style : import 및 code format수정
    - 세부적인 import 확인할 수 있도록 변경
    - 컨벤션이 맞지 않은 단락 부분 변경

--- src/main/java/com/sns/yourconnection/controller/UserPublicApiController.java
+++ src/main/java/com/sns/yourconnection/controller/UserPublicApiController.java
@@ -15,7 +15,13 @@ import com.sns.yourconnection.service.thirdparty.email.SmtpMailService;
 import com.sns.yourconnection.service.thirdparty.oauth2.OAuth2LoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import static com.sns.yourconnection.controller.response.ResponseSuccess.response;
 
@@ -84,5 +90,4 @@ public class UserPublicApiController {
 
         return response(UserLoginResponse.of(accessToken));
     }
-
 }
